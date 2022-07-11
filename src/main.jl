6d3a1f2682285c700ac3682b6cee2b1154bd3c94
Markus Petters <mdpetter@ncsu.edu>
2022-07-11 13:11:17 +0200

    fix paths for Windows

--- src/main.jl
+++ src/main.jl
@@ -33,7 +33,12 @@ cr = Signal(1.0)
 
 include("greeter.jl")
 
-bp = dir.value * "/"
+if Sys.iswindows()
+    bp = dir.value * "\\"
+else
+    bp = dir.value * "/"
+end
+
 files, t, T = extract_filename_data(bp)      # get selected files, times, and temperatures
 
 include("dropdetection.jl")
@@ -47,9 +52,26 @@ ims = validatestack(freezeindex)             # frames for validation in GUI
 include("dropvalidation.jl")
 
 # Level 2 output data
-a = split(bp, "/")
-outpath = mapfoldl(i -> String(a[i])*"/", *, 1:length(a)-3) * "level 2/" * String(a[end-1]) * "/"
+if Sys.iswindows()
+    a = split(bp, "\\")
+    outpath =
+        mapfoldl(i -> String(a[i]) * "\\", *, 1:length(a)-3) *
+        "level 2\\" *
+        String(a[end-1]) *
+        "\\"
+    minipath = split(dir.value, "\\") |> x -> String(x[end-1]) * "\\" * String(x[end])
+else
+    a = split(bp, "/")
+    outpath =
+        mapfoldl(i -> String(a[i]) * "/", *, 1:length(a)-3) *
+        "level 2/" *
+        String(a[end-1]) *
+        "/"
+    minipath = split(dir.value, "/") |> x -> String(x[end-1]) * "/" * String(x[end])
+end
+
 read(`mkdir -p $outpath`)
+
 ts = t[freezeindex[ii.==true]]
 Ts = T[freezeindex[ii.==true]]
 cs = centers[ii.==true]
@@ -58,17 +80,42 @@ summary_image = map(i -> hcat(i...), tstack) |> x -> vcat(x...)
 discardedimg = vcat(triplets[ii.==false]...)
 acceptimg = vcat(triplets[ii.==true]...)
 xs = map(c -> c[1], cs)
-ys = map(c -> c[2], cs) 
+ys = map(c -> c[2], cs)
 df = @> DataFrame(T = Ts, timestamp = ts, xpos = xs, ypos = ys) sort(rev = true)
 
 inp = INP(Ts, dropV.value)
-p1 = plot(inp.T, inp.Cin, yscale = :log10, ylim = (1e3, 1e7), xlim = (-35, 0), color = :black, label = "", xlabel = "Temperature (°C)", ylabel = "INP (# L⁻¹)")
-p2 = plot(inp.T, inp.f, xlim = (-35, 0), xlabel = "Temperature (°C)", ylabel = "Activated fraction (-)", color = :black, label = "")
-p = plot(p1, p2, layout = grid(1,2), size = (700,300), left_margin = 20px, bottom_margin = 20px, left_largin = 10px)
+p1 = plot(
+    inp.T,
+    inp.Cin,
+    yscale = :log10,
+    ylim = (1e3, 1e7),
+    xlim = (-35, 0),
+    color = :black,
+    label = "",
+    xlabel = "Temperature (°C)",
+    ylabel = "INP (# L⁻¹)",
+)
+p2 = plot(
+    inp.T,
+    inp.f,
+    xlim = (-35, 0),
+    xlabel = "Temperature (°C)",
+    ylabel = "Activated fraction (-)",
+    color = :black,
+    label = "",
+)
+p = plot(
+    p1,
+    p2,
+    layout = grid(1, 2),
+    size = (700, 300),
+    left_margin = 20px,
+    bottom_margin = 20px,
+    left_largin = 10px,
+)
 
-minipath = split(dir.value, "/") |> x -> String(x[end-1]) * "/" * String(x[end])
 
-open(outpath*"metadata.txt", "w") do f
+open(outpath * "metadata.txt", "w") do f
     println(f, "NC State Cold-Stage: Metadata File")
     @>> @sprintf("Raw Data Directory: %s", minipath) println(f)
     @>> @sprintf("Sample Description: %s", sample.value) println(f)
@@ -79,14 +126,14 @@ open(outpath*"metadata.txt", "w") do f
     @>> @sprintf("Number Accepted Drops: %i", length(Ts)) println(f)
 end
 
-FileIO.save(outpath*"dropmoasic.jpg", summary_image)
-FileIO.save(outpath*"dropcenters.jpg", drawcircles(cs, image))
-FileIO.save(outpath*"accepteddrops.jpg", acceptimg)
+FileIO.save(outpath * "dropmoasic.jpg", summary_image)
+FileIO.save(outpath * "dropcenters.jpg", drawcircles(cs, image))
+FileIO.save(outpath * "accepteddrops.jpg", acceptimg)
 if size(discardedimg)[1] > 10
-    FileIO.save(outpath*"discardedrops.jpg", discardedimg)
+    FileIO.save(outpath * "discardedrops.jpg", discardedimg)
 end
-df |> CSV.write(outpath*"dropdata.csv")
-inp |> CSV.write(outpath*"spectrum.csv")
-savefig(p, outpath*"quicklook.png")
+df |> CSV.write(outpath * "dropdata.csv")
+inp |> CSV.write(outpath * "spectrum.csv")
+savefig(p, outpath * "quicklook.png")
 
 include("exit.jl")
